222b2faede720a4667631f85c323acd66aa59310
Karfann Nacif <karfannacif@gmail.com>
2017-12-13 00:02:35 +0000

    Intregrated new and create actions for portifolios

--- app/controllers/portifolios_controller.rb
+++ app/controllers/portifolios_controller.rb
@@ -3,4 +3,29 @@ class PortifoliosController < ApplicationController
     def index
         @portifolio_items = Portifolio.all
     end
+
+    def new
+        @portifolio_item = Portifolio.new
+    end
+
+    def create
+        @portifolio_item = Portifolio.new(portifolio_params)
+    
+        respond_to do |format|
+          if @portifolio_item.save
+            format.html { redirect_to portifolios_path, notice: 'Portifolio was successfully created.' }
+            format.json { render :show, status: :created, location: @portifolio_item }
+          else
+            format.html { render :new }
+            format.json { render json: @portifolio_item.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+
+      private
+
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def portifolio_params
+            params.require(:portifolio).permit(:title, :subtitle, :body)
+        end
 end

fe98ab6c831c19932100752f1cd599d4bb47c078
Karfann Nacif <karfannacif@gmail.com>
2017-12-22 12:23:40 +0000

    Implemented customs scopes

--- app/models/portifolio.rb
+++ app/models/portifolio.rb
@@ -1,3 +1,6 @@
 class Portifolio < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :thumb_image
+
+    scope :ruby_on_rails_portifolio_items, -> { where(subtitle: 'Ruby on Rails' )}
+    scope :angular_portifolio_items, -> { where(subtitle: 'Angular' )}
 end

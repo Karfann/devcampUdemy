d6b3a4f1153871b6724b969bbdf7461c829b9a95
Karfann Nacif <karfannacif@gmail.com>
2018-01-16 09:31:17 +0000

    Implemented partials with local data for portifolio form and nav bar

--- app/controllers/portifolios_controller.rb
+++ app/controllers/portifolios_controller.rb
@@ -4,68 +4,68 @@ class PortifoliosController < ApplicationController
 
   layout 'portifolio'
 
-    def index
-        @portifolio_items = Portifolio.all
-    end
+  def index
+      @portifolio_items = Portifolio.all
+  end
 
-    def new
-        @portifolio_item = Portifolio.new
-        3.times { @portifolio_item.technologies.build}
-    end
+  def new
+      @portifolio_item = Portifolio.new
+      3.times { @portifolio_item.technologies.build}
+  end
 
-    def create
-        @portifolio_item = Portifolio.new(portifolio_params)
-    
-        respond_to do |format|
-          if @portifolio_item.save
-            format.html { redirect_to portifolios_path, notice: 'Portifolio was successfully created.' }
-            format.json { render :show, status: :created, location: @portifolio_item }
-          else
-            format.html { render :new }
-            format.json { render json: @portifolio_item.errors, status: :unprocessable_entity }
-          end
-        end
+  def create
+    @portifolio_item = Portifolio.new(portifolio_params)
+
+    respond_to do |format|
+      if @portifolio_item.save
+        format.html { redirect_to portifolios_path, notice: 'Portifolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portifolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portifolio_item.errors, status: :unprocessable_entity }
       end
-
-      def edit
-      end
-
-    def update
-        
-        respond_to do |format|
-          if @portifolio_item.update(portifolio_params)
-            format.html { redirect_to portifolios_path, notice: 'The record successfully updated.' }
-            format.json { render :show, status: :ok, location: @portifolio_item }
-          else
-            format.html { render :edit }
-            format.json { render json: @portifolio_item.errors, status: :unprocessable_entity }
-          end
-        end
     end
+  end
 
-    def destroy
-      @portifolio_item.destroy
+  def edit
+  end
+
+  def update
+      
       respond_to do |format|
-        format.html { redirect_to portifolios_url, notice: 'Portifolio was successfully destroyed.' }
-        format.json { head :no_content }
+        if @portifolio_item.update(portifolio_params)
+          format.html { redirect_to portifolios_path, notice: 'The record successfully updated.' }
+          format.json { render :show, status: :ok, location: @portifolio_item }
+        else
+          format.html { render :edit }
+          format.json { render json: @portifolio_item.errors, status: :unprocessable_entity }
+        end
       end
+  end
+
+  def destroy
+    @portifolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portifolios_url, notice: 'Portifolio was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def show
+  end
+
+  private
+
+    def set_portifolio
+      @portifolio_item = Portifolio.find(params[:id])
     end
 
-    def show
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def portifolio_params
+        params.require(:portifolio).permit( :title, 
+                                            :subtitle, 
+                                            :body, 
+                                            technologies_attributes: [:name]
+                                          )
     end
-
-      private
-
-        def set_portifolio
-          @portifolio_item = Portifolio.find(params[:id])
-        end
-
-        # Never trust parameters from the scary internet, only allow the white list through.
-        def portifolio_params
-            params.require(:portifolio).permit( :title, 
-                                                :subtitle, 
-                                                :body, 
-                                                technologies_attributes: [:name]
-                                              )
-        end
 end
